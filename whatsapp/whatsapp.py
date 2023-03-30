efd7faf89ec93b39dc3c5d88b3e543c19ceae077
xuanli286 <xuanli743@gmail.com>
2023-03-30 14:13:56 +0800

    updated message content in whatsapp

--- whatsapp/whatsapp.py
+++ whatsapp/whatsapp.py
@@ -25,25 +25,25 @@ def sendWhatsapp(data):
     ownerPhone = 'whatsapp:+65' + data['ownerPhone']
     renterPhone = 'whatsapp:+65' + data['renterPhone']
 
-    # data = {'bookingStatus':'pending', 'title':'Hideout Horizon - Eco Bamboo Home', 'price': 386, 'ownerFullname':'Low Xuanli', 'ownerPhone':'98242683', 'renterFullname':'Germaine Tan', 'renterPhone':'98242683'}
+    # data = {'bookingStatus':'pending', 'bookingid':'1', 'ownerFullname':'Low Xuanli', 'ownerPhone':'98242683', 'renterFullname':'Germaine Tan', 'renterPhone':'98242683'}
     if data['bookingStatus'] == 'pending':
-        ownerMsg = f"[no reply] From Travelnest: Hi {data['ownerFullname']}, there is a booking placed at {data['title']} pending for your approval. Please login to view the booking."
-        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, thank you for booking {data['title']}! Your payment of S${data['price']} is currently kept on hold and you will receive the status of your booking within 3 working days."
+        ownerMsg = f"[no reply] From Travelnest: Hi {data['ownerFullname']}, there is a booking BookingID: {data['bookingid']} awaiting your approval. Please login to view the booking."
+        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, thank you for booking with us! Your payment is currently kept on hold and you will receive the status of your booking BookingID: {data['bookingid']} within 3 working days."
         client.messages.create(to=ownerPhone, from_='whatsapp:+14155238886', body=ownerMsg) 
         client.messages.create(to=renterPhone, from_='whatsapp:+14155238886', body=renterMsg) 
     
-    # data = {'bookingStatus':'cancelled', 'title':'Hideout Horizon - Eco Bamboo Home', 'price':386, 'ownerFullname':'Low Xuanli', 'ownerPhone':'98242683', 'renterFullname':'Germaine Tan', 'renterPhone':'87276883'}
+    # data = {'bookingStatus':'confirmed', 'bookingid':'1', 'ownerFullname':'', 'ownerPhone':'',  'renterFullname':'Low Xuanli', 'renterPhone':'98242683'}
     elif data['bookingStatus'] == 'confirmed':
-        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, your booking at {data['title']} is confirmed and the payment of S${data['price']} has been deducted successfully. You may login to view your booking(s)."
+        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, your booking BookingID: {data['bookingid']} is confirmed and the payment has been deducted successfully. You may login to view your booking(s)."
         client.messages.create(to=renterPhone, from_='whatsapp:+14155238886', body=renterMsg) 
     
     elif data['bookingStatus'] == 'rejected':
-        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, sorry to inform you that your booking at {data['title']} is not successful and the payment of S${data['price']} has been refunded."
+        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, sorry to inform you that your booking BookingID: {data['bookingid']} is not successful and the payment has been refunded."
         client.messages.create(to=renterPhone, from_='whatsapp:+14155238886', body=renterMsg) 
 
     else:
-        ownerMsg = f"[no reply] From Travelnest: Hi {data['ownerFullname']}, the booking placed at {data['title']} has been cancelled."
-        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, your booking at {data['title']} has been cancelled and the payment of S${data['price']} has been refunded."
+        ownerMsg = f"[no reply] From Travelnest: Hi {data['ownerFullname']}, the booking BookingID: {data['bookingid']} has been cancelled."
+        renterMsg = f"[no reply] From Travelnest: Hi {data['renterFullname']}, your booking BookingID: {data['bookingid']} has been cancelled and the payment has been refunded."
         client.messages.create(to=ownerPhone, from_='whatsapp:+14155238886', body=ownerMsg) 
         client.messages.create(to=renterPhone, from_='whatsapp:+14155238886', body=renterMsg) 
 
