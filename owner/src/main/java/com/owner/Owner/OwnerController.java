b175a314ce264ee7f7c0c2ec393ef175f487c9c3
xuanli286 <xuanli743@gmail.com>
2023-04-01 00:28:39 +0800

    added cross origin for renter and owner

--- owner/src/main/java/com/owner/Owner/OwnerController.java
+++ owner/src/main/java/com/owner/Owner/OwnerController.java
@@ -2,16 +2,14 @@ package com.owner.Owner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/owner")
 public class OwnerController {
--- renter/src/main/java/com/renter/Renter/RenterController.java
+++ renter/src/main/java/com/renter/Renter/RenterController.java
@@ -2,16 +2,14 @@ package com.renter.Renter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/renter")
 public class RenterController {
